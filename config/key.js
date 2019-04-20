19bcc1aedbdcad0cefeef6fa15acc702fb91a893
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-20 15:46:08 -0600

    update

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Piper:colorado@mycluster-9chet.mongodb.net/test?retryWrites=true",
+    secretOrKey: "secret"
+}
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+//initialize express
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/key").mongoURI;
+
+// const mongoURI = require("./config/key");
+// const db = mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require("./config/passport") (passport);
+
+//routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server is now
+ running on port ${port} !`));
