43c6df301cb109e11e98895e6bb294b7cc4cab66
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-22 14:39:28 -0600

    update

--- config/key.js
+++ config/key.js
@@ -1,4 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb+srv://Piper:colorado@mycluster-9chet.mongodb.net/test?retryWrites=true",
+    mongoURI: "mongodb://testuser:testing1@ds237855.mlab.com:37855/heroku_5l3w10xr",
     secretOrKey: "secret"
 }
+
+// mongodb+srv://Piper:colorado@mycluster-9chet.mongodb.net/test?retryWrites=true"
--- validation/register.js
+++ validation/register.js
@@ -1,28 +1,28 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
-module.exports = function validateRegisterInput (data) {
+module.exports = function validateRegisterInput(data) {
     let errors = {};
 
-    //convert empty fields to an empty string so we can use validator functions
+    // Convert empty fields to an empty string so we can use validator functions
     data.name = !isEmpty(data.name) ? data.name : "";
     data.email = !isEmpty(data.email) ? data.email : "";
     data.password = !isEmpty(data.password) ? data.password : "";
-    data.password2  = !isEmpty(data.password2) ? data.password2 : "";
+    data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-    //check the name input here
+    // Name checks
     if (Validator.isEmpty(data.name)) {
         errors.name = "Name field is required";
     }
 
-    //check the email input here
+    // Email checks
     if (Validator.isEmpty(data.email)) {
         errors.email = "Email field is required";
     } else if (!Validator.isEmail(data.email)) {
         errors.email = "Email is invalid";
     }
 
-    //check the password input here
+    // Password checks
     if (Validator.isEmpty(data.password)) {
         errors.password = "Password field is required";
     }
@@ -31,11 +31,14 @@ module.exports = function validateRegisterInput (data) {
         errors.password2 = "Confirm password field is required";
     }
 
-    if (!Validator.isLength(data.password, {min: 6, max: 30 })) {
+    if (!Validator.isLength(data.password, {
+            min: 6,
+            max: 30
+        })) {
         errors.password = "Password must be at least 6 characters";
     }
 
-    if(!Validator.equals(data.password, data.password2)) {
+    if (!Validator.equals(data.password, data.password2)) {
         errors.password2 = "Passwords must match";
     }
 
